416ac89d066bab70741e1b5b06dde9667818e9cb
simon <simon@sig.tw>
2018-09-10 23:48:32 +0800

    add new post alert

---
+++ readme.txt
@@ -0,0 +1,45 @@
+=== WP LINE Notify ===
+Contributors: simon_tw
+Tags: plugin, line
+Requires at least: 4.0
+Tested up to: 4.9.8
+Requires PHP: 5.6
+Stable tag: 0.1.3
+License: GPLv3
+License URI: https://www.gnu.org/licenses/gpl-3.0.html
+
+This plugin can send a alert message by LINE Notify.
+(利用 LINE Notify 傳送警示訊息給你)
+
+== Description ==
+
+This plugin can send a alert message by LINE Notify
+(利用 LINE Notify 傳送警示訊息給你)
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+e.g.
+
+1. Upload the plugin files to the `/wp-content/plugins/plugin-name` directory, or install the plugin through the WordPress plugins screen directly.（將外掛上傳至目錄 /wp-content/plugins/ 下，或是透過後台安裝外掛）
+2. Activate the plugin through the `Plugins` screen in WordPress.（在外掛清單中啟用本外掛）
+
+== Screenshots ==
+
+1. screenshot-1.png
+2. screenshot-2.png
+
+== Changelog ==
+
+= 0.1.3 - 2018-09-10 =
+* Add send message for new post
+
+= 0.1.2 - 2018-09-07 =
+* Fixed bug
+
+= 0.1.1 - 2018-09-05 =
+* Added new notify for Contact Form 7
+
+= 0.1.0 - 2018-09-03 =
+* Initial Public Release
--- wp-line-notify.php
+++ wp-line-notify.php
@@ -3,7 +3,7 @@
  * Plugin Name: WordPress LINE Notify
  * Plugin URI:  https://github.com/mark2me/wp-line-notify
  * Description: This plugin can send a alert message by LINE Notify
- * Version:     0.1.1
+ * Version:     0.1.3
  * Author:      Simon Chuang
  * Author URI:  https://github.com/mark2me
  * License:     GPLv2
@@ -36,11 +36,12 @@ class sig_line_notify{
         $this->plugin_name = SIG_LINE_NOTIFY_PLUGIN_NAME;
         $this->options = get_option(SIG_LINE_NOTIFY_OPTIONS);
 
-        // add plugin menu
+        // add menu
         add_action( 'admin_menu', array($this,'add_option_menu') );
 
         add_filter("plugin_action_links_".plugin_basename(__FILE__) ,array($this, 'plugin_settings_link') );
 
+        // add_action
         if( isset($this->options['comments']) && $this->options['comments'] == 1 ){
             add_action( 'comment_post' , array($this, 'new_comments_alert') , 10 ,2  );
         }
@@ -48,7 +49,7 @@ class sig_line_notify{
         if( isset($this->options['woocommerce']) && $this->options['woocommerce'] == 1 ){
             include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
             if(is_plugin_active( 'woocommerce/woocommerce.php' )) {
-                add_action( 'woocommerce_new_order', array($this,'new_woocommerce_order_alert') , 10, 3 );
+                add_action( 'woocommerce_checkout_update_order_meta', array($this,'new_woocommerce_order_alert') , 10, 3 );
 	        }
 	    }
 
@@ -60,12 +61,15 @@ class sig_line_notify{
             add_action("wpcf7_before_send_mail", array($this, "new_wpcf7_message"));
         }
 
-     }
+        if( isset($this->options['new_post']) && count($this->options['new_post']) > 0 ){
+            add_action('wp_insert_post',array($this,'new_post_alert'),10,3);
+        }
+    }
 
     function add_option_menu(){
         add_options_page(
             __('Line Notify Setting', $this->plugin_name),
-            __('Line Notify', $this->plugin_name),
+            __('WP Line Notify', $this->plugin_name),
             'administrator',
             'sig-'.$this->plugin_name,
             array($this, 'html_settings_page')
@@ -91,77 +95,12 @@ class sig_line_notify{
             $test_send = ($send) ? '<div class="notice notice-success is-dismissible"><p>'. __( 'Send test', $this->plugin_name ) .'</p></div>' : '<div class="notice notice-error is-dismissible"><p>'. __( 'Error on send LINE Notify.',$this->plugin_name ) .'</p></div>';
         }
 
-    ?>
-        <div class="wrap">
-            <h2><?php _e('Line Notify Setting',$this->plugin_name)?></h2>
-            <form method="post" action="options.php">
-                <?php settings_fields('line-notify-option'); ?>
-                <table class="form-table">
-                    <tr valign="top">
-                        <th scope="row"><?php _e('Line Notify Token:',$this->plugin_name)?></th>
-                        <td>
-                            <input type="text" class="regular-text" name="<?php echo SIG_LINE_NOTIFY_OPTIONS?>[token]" value="<?php echo esc_attr( $this->options['token'] ) ?>">
-                            <p class="description">到 <a href="https://notify-bot.line.me/my/" target="_blank">LINE Notify</a> 申請發行存取權杖。 </p>
-                        </td>
-                    </tr>
-
-                    <tr valign="top">
-                        <th scope="row"><?php _e('comments:', $this->plugin_name)?></th>
-                        <td>
-                            <input type="checkbox" id="chcek_comment" name="<?php echo SIG_LINE_NOTIFY_OPTIONS?>[comments]" value="1" <?php echo checked( 1, $this->options['comments'], false )?>>
-                            <label for="chcek_comment"><?php _e('Add new comment.', $this->plugin_name)?></label>
-                        </td>
-                    </tr>
-
-                    <tr valign="top">
-                        <th scope="row"><?php _e('Woocommerce:', $this->plugin_name)?></th>
-                        <td>
-                            <input type="checkbox" id="chcek_order" name="<?php echo SIG_LINE_NOTIFY_OPTIONS?>[woocommerce]" value="1" <?php echo checked( 1, $this->options['woocommerce'], false )?>>
-                            <label for="chcek_order"><?php _e('Add new order.', $this->plugin_name)?></label><?php
-                                include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
-                                if(!is_plugin_active( 'woocommerce/woocommerce.php' )) echo "&nbsp;&nbsp;<p class=\"description\">(".__('This plugin is not install or active.', $this->plugin_name).")</p>";
-                            ?>
-                        </td>
-                    </tr>
-
-                    <tr valign="top">
-                        <th scope="row"><?php _e('User:', $this->plugin_name)?></th>
-                        <td>
-                            <input type="checkbox" id="chcek_user" name="<?php echo SIG_LINE_NOTIFY_OPTIONS?>[user_register]" value="1" <?php echo checked( 1, $this->options['user_register'], false )?>>
-                            <label for="chcek_user"><?php _e('An user join.', $this->plugin_name)?></label>
-                        </td>
-                    </tr>
-
-                    <tr valign="top">
-                        <th scope="row"><?php _e('Contact Form 7:', $this->plugin_name)?></th>
-                        <td>
-                            <input type="checkbox" id="chcek_cf7" name="<?php echo SIG_LINE_NOTIFY_OPTIONS?>[wpcf7]" value="1" <?php echo checked( 1, $this->options['wpcf7'], false )?>>
-                            <label for="chcek_cf7"><?php _e('An new contact message.', $this->plugin_name)?></label><?php
-                                if(!is_plugin_active( 'contact-form-7/wp-contact-form-7.php' )) echo "&nbsp;&nbsp;<p class=\"description\">(".__('This plugin is not install or active.', $this->plugin_name).")</p>";
-                            ?>
-                        </td>
-                    </tr>
-
-                </table>
-                <?php submit_button(); ?>
-            </form>
-
-            <h2><?php _e('Test Line Notify', $this->plugin_name)?></h2>
-            <?php
-            if(isset($test_send)) echo $test_send;
-            ?>
-            <form action="" method="post">
-            <?php wp_nonce_field('test_button_clicked'); ?>
-                <textarea name="text_line_notify" style="width: 500px; height: 100px; "></textarea>
-                <?php submit_button('Test'); ?>
-            </form>
-        </div>
-    <?
+        require_once plugin_dir_path( __FILE__ ) . 'inc/page_setup.php';
     }
 
 
     /*
-        send
+        Send message
     */
     function line_send($text) {
 
@@ -189,7 +128,7 @@ class sig_line_notify{
 
     	if( isset($this->options['comments']) && $this->options['comments'] == 1 ){
         	$comment = get_comment( $comment_ID );
-        	$message = __("You have an new comment.\n" , $this->plugin_name ) . $comment->comment_content;
+        	$message = __("You have a new comment.\n" , $this->plugin_name ) . $comment->comment_content;
     		$this->line_send( $message );
     	}
     }
@@ -201,8 +140,19 @@ class sig_line_notify{
             $order = wc_get_order( $order_get_id );
             $order_data = $order->get_data();
 
-        	$message = __( "You have an new order." , $this->plugin_name );
-        	if(isset($order_data['total'])) $message .= __( " total :" , $this->plugin_name ) . $order_data['total'];
+
+        	$message = __( "You have a new order." , $this->plugin_name );
+
+            if(isset($order_data['line_items']) && count($order_data['line_items'])>0){
+                foreach($order_data['line_items'] as $item){
+                    if( isset($item['name']) && isset($item['quantity']) ){
+                        $message .= "\n {$item['name']} x {$item['quantity']}";
+                    }
+                }
+            }
+
+        	if(isset($order_data['total'])) $message .= "\n".__( " total :" , $this->plugin_name ) . $order_data['total'];
+
     		$this->line_send( $message );
     	}
 
@@ -211,7 +161,7 @@ class sig_line_notify{
     function new_user_register_alert( $user_id ) {
 
         if( isset($this->options['user_register']) && $this->options['user_register'] == 1 ){
-            $message = __( "You have an new user register." , $this->plugin_name );
+            $message = __( "You have a new user register." , $this->plugin_name );
 
             $user_info = get_userdata($user_id);
             $message .= __( " Username: " , $this->plugin_name ) . $user_info->user_login;
@@ -219,13 +169,60 @@ class sig_line_notify{
         }
     }
 
-
     function new_wpcf7_message($cf7) {
 
         $wpcf = WPCF7_ContactForm::get_current();
 
-        $message = __( "You have an new contact message." , $this->plugin_name );
-        $this->line_send( $message );
+        $message = __( "You have a new contact message." , $this->plugin_name );
+        $this->line_send( $message . serialize($wpcf));
+    }
+
+    function new_post_alert($post_id, $post, $update){
+
+
+        if ( wp_is_post_revision( $post_id ) ) return;
+
+        if( isset($this->options['new_post']) && count($this->options['new_post']) > 0 ){
+            foreach($this->options['new_post'] as $name => $sel){
+                $allow_role[] = $name;
+            }
+        }else{
+            return;
+        }
+
+        if( is_object($post) && ($post->post_status == 'publish' || $post->post_status == 'pending') ){
+
+            if($post->post_type !== 'post') return;
+
+            $post_title = $post->post_title;
+            $post_link  = $post->guid;
+            $post_type  = $post->post_type;
+            $uid        = $post->post_author;
+
+            $user = get_userdata($uid);
+
+            if(is_object($user)){
+
+                $user_name = $user->display_name;
+                $role = (array)$user->roles;
+                $role_name = $role[0];
+
+                if(in_array($role_name,$allow_role)){
+
+                    $message = $user_name;
+
+                    if( $post->post_status == 'pending' ){
+                        $message .= __( " add a pending post." , $this->plugin_name );
+                    }else{
+                        $message .= __( " add a new post." , $this->plugin_name );
+                    }
+
+                    $message .= " $post_title $post_link" ;
+                    $this->line_send( $message );
+                }
+            }
+        }
+
     }
 
 }
